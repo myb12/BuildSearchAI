97536700073036240e2e5e23140b652ef6ab2d1b
Mohammad Yasin <mohammadyasinbappy@outlook.com>
2025-07-22 23:17:55 +0600

    login route created

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from 'next/server';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import prisma from '../../../lib/db';
+
+export async function POST(request: Request) {
+  try {
+    const { email, password } = await request.json();
+
+    if (!email || !password) {
+      return NextResponse.json({ message: 'Email and password are required' }, { status: 400 });
+    }
+
+    // Find the user by email
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    // return NextResponse.json(
+    //   { user },
+    //   { status: 200 } 
+    // );
+
+    if (!user) {
+      return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
+    }
+
+    // Compare provided password with hashed password
+    const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
+
+    if (!isPasswordValid) {
+      return NextResponse.json({ message: 'Invalid credentials' }, { status: 401 });
+    }
+
+    // Generate JWT
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      process.env.JWT_SECRET!,
+      { expiresIn: '1h' }
+    );
+
+    const { passwordHash, ...userWithoutPassword } = user;
+
+    return NextResponse.json({ message: 'Login successful', token, user: userWithoutPassword }, { status: 200 });
+
+  } catch (error) {
+    console.error('Login error:', error);
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+  }
+}
